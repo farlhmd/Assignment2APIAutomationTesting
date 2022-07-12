48061d669a2735d39a9d5e94ff0adbce8cbce089
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-07-12 21:04:08 +0700

    Update Get all photos from specific album.rs

--- Object Repository/EP_Albums/Get all photos from specific album.rs
+++ Object Repository/EP_Albums/Get all photos from specific album.rs
@@ -15,7 +15,7 @@
    <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>${GlobalVariable.url}/albums/${id}/photos</restUrl>
+   <restUrl>${GlobalVariable.url}/albums/${GlobalVariable.id}/photos</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
